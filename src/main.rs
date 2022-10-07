1fd53b0e242bf4ef935360e6722969e56942ae9c
wat-aro <kazutas1008@gmail.com>
2022-10-07 23:25:43 +0900

    Determine if stdin is pipe or terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    io::{self, stdout, Write},
+};
+
+use nix::{libc::STDIN_FILENO, unistd::isatty};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut buffer = String::new();
+    if let Ok(true) = isatty(STDIN_FILENO) {
+        print!("risp> ");
+        stdout().flush().unwrap();
+    }
+
+    let stdin = io::stdin();
+    stdin.read_line(&mut buffer)?;
+    print!("{}", buffer);
+    Ok(())
 }
