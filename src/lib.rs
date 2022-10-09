3148a1c146afdf2c47df1df9d40286bec86dedab
wat-aro <kazutas1008@gmail.com>
2022-10-09 22:53:19 +0900

    repl

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,31 @@
 mod expr;
 
-use std::{error::Error, io};
+use std::io;
 
+use anyhow::{bail, Result};
 use expr::{Expr, Expr::Integer};
 
-pub fn read() -> Result<Expr, Box<dyn Error>> {
+pub fn read() -> Result<Expr> {
     let mut buffer = String::new();
     let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
-    let integer = buffer.trim().parse::<i64>()?;
-    Ok(Integer(integer))
+    match stdin.read_line(&mut buffer) {
+        Ok(n) => {
+            if n == 0 {
+                // EOF given
+                bail!("EOF");
+            } else {
+                let integer = buffer.trim().parse::<i64>()?;
+                Ok(Integer(integer))
+            }
+        }
+        Err(e) => bail!(e),
+    }
+}
+
+pub fn eval(expr: Expr) -> Expr {
+    expr
+}
+
+pub fn print_expr(expr: Expr) {
+    println!("{}", expr);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 use std::{
-    error::Error,
     io::{stdout, Write},
+    process::exit,
 };
 
+use anyhow::Result;
 use nix::{libc::STDIN_FILENO, unistd::isatty};
-use risp::read;
+use risp::{eval, print_expr, read};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     if let Ok(true) = isatty(STDIN_FILENO) {
+        read_eval_print_loop()?;
+    } else {
+        let expr = read()?;
+        eval(expr);
+    }
+    Ok(())
+}
+
+fn read_eval_print_loop() -> Result<()> {
+    print!("risp> ");
+    stdout().flush().unwrap();
+
+    loop {
+        match read() {
+            Ok(expr) => {
+                let expr = eval(expr);
+                print_expr(expr);
+            }
+            Err(ref error) if error.to_string() == "EOF" => {
+                exit(1);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+            }
+        };
         print!("risp> ");
         stdout().flush().unwrap();
     }
-
-    let result = read()?;
-    println!("{}", result);
-    Ok(())
 }
